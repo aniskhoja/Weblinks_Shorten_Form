4de9d998bfd50f803dee255817befa4fcd260051
aniskhoja <anis.khojaa@gmail.com>
2021-12-06 01:02:36 -0800

    Pagination added

--- index.js
+++ index.js
@@ -12,10 +12,31 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', async (req, res) => {
-    const allUrl = await Url.find()
-    res.render('index', { url: allUrl})
+   
+    const limit = parseInt(req.query.limit) || 2;
+    const page = parseInt(req.query.page) || 1;
 
-    
+
+    const startIndex = (page - 1)*limit
+    const endIndex = page * limit
+       
+    const result = {}
+    if(endIndex < await Url.countDocuments().exec()){
+        result.next = {
+            page : page + 1,
+            limit : limit
+        }
+    }
+
+    if(startIndex > 0){
+        result.previous = {
+            page : page -1,
+            limit : limit
+        }
+    }
+    const allUrl = await Url.find();
+    const urlList = await Url.find().limit(limit).skip(startIndex)
+    res.render('index', data = {urlAll : allUrl, url: urlList, next : result.next, previous: result.previous})  
 }),
 
 app.post('/url', async (req, res) => {
@@ -23,7 +44,7 @@ app.post('/url', async (req, res) => {
         url: req.body.url
     })
 
-    res.redirect('/')
+    res.redirect('/?page=1&limit=4')
 })
 
 app.get('/:url', async (req, res) => {
